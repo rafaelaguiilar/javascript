19b5b2cbcff8c0fb247c1b660d64c8eeaeb5cb66
rafaelaguiilar <rafaelaguilarthai@hotmail.com>
2023-05-12 07:32:21 -0300

    ex015 criado

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,15 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    //var hora = 19
     msg.innerHTML = `Agora são ${hora} Horas.`
     if (hora >= 0 && hora < 12) {
-            // BOM DIA!
-            img.src = 'fotomanha.png'
-    } else if (hora >= 12 && hora < 18) {
-            // BOA TARDE!
-            img.src = 'fototarde.png'
+        // BOM DIA!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#c19d5f'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'fototarde.png'
+        document.body.style.background = '#985d35'
     } else {
-            //BOA NOITE!  
-            img.src = 'fotonoite.png'  
+        //BOA NOITE!  
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#40595a'  
     }
 }
